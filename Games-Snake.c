82975e9c39059859a466979688aa77ff5097099f
raccoon god <raccoon1234@naver.com>
2018-05-24 19:32:44 +0900

    맵전체툴 수정

--- Games-Snake.c
+++ Games-Snake.c
@@ -11,15 +11,14 @@
 /*
 Config file:  config.cfg
 content:
-
 arraySizeX               16 // these two values should be equal
 arraySizeY               16 // no bigger than 16
 StartingDirection         2  // 0 - random.  1 - nadqsno, 2 - nagore, 3 -nalqvo, 4, nadolu
 snakeSize               5  // no longer than 5;
 IncreaseSpeedOnEveryFood   1
-
 The config value is opened only if the user choose '0' for the size of the array.
 */
+
 int main()
 {
 	FILE *fp;
@@ -52,49 +51,65 @@ int main()
 	COORD pos = { 0, 0 };
 	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	while (1)      //초기 설정부분
-	{
-		fflush(stdin);
-		printf("Game window size (16 - 25)\nEnter '0' to use config file values");
-		scanf("%d", &arraySizeX);
+	//while (1)                 //초기 스테이지 크기 설정
+	//{
+	//   fflush(stdin);
+	//   printf("Game window size (16 - 25)\nEnter '0' to use config file values");
+	//   scanf("%d", &arraySizeX);
 
-		if ((!(arraySizeX < 16 || arraySizeX > 25)) || (arraySizeX == 0)) break;
-	}
+	//   if ((!(arraySizeX < 16 || arraySizeX > 25)) || (arraySizeX == 0)) break;
+	//}
+
+	Speed = 5;
+	arraySizeX = 20;
 	arraySizeY = arraySizeX;
 
-	if (arraySizeX == 0)   //디폴트 파일 설정 불러오기
-	{
-		ChetemConfig = 1;
-		fp = fopen("config.cfg", "rt");
-		if (fp == NULL)
-		{
-			printf("Cannot find config file. Check existance or start new game and choose value between 16-25 for window game size\nGame will now exit.");
-			_getch();
-			return 0;
-		}
-		for (i = 0; i < NUMBER_OF_CONFIG_ELEMENTS; i++)
-		{
-			if (i == 0) fscanf(fp, "%s%d", unused, &arraySizeX);
-			if (i == 1) fscanf(fp, "%s%d", unused, &arraySizeY);
-			if (i == 2) fscanf(fp, "%s%d", unused, &STARTING_DIRECTION);
-			if (i == 3) fscanf(fp, "%s%d", unused, &INITIAL_SNAKE_SIZE);
-			if (i == 4) fscanf(fp, "%s%d", unused, &INCREASE_SPEED_ON_EVERY_FOOD);
-		}
-		fclose(fp);
-	}
+	//if (arraySizeX == 0)          //디폴트파일 설정 불러오기
+	//{
+	//   ChetemConfig = 1;
+	//   fp = fopen("config.cfg", "rt");
+	//   if (fp == NULL)
+	//   {
+	//      printf("Cannot find config file. Check existance or start new game and choose value between 16-25 for window game size\nGame will now exit.");
+	//      _getch();
+	//      return 0;
+	//   }
+	//   for (i = 0; i < NUMBER_OF_CONFIG_ELEMENTS; i++)
+	//   {
+	//      if (i == 0) fscanf(fp, "%s%d", unused, &arraySizeX);
+	//      if (i == 1) fscanf(fp, "%s%d", unused, &arraySizeY);
+	//      if (i == 2) fscanf(fp, "%s%d", unused, &STARTING_DIRECTION);
+	//      if (i == 3) fscanf(fp, "%s%d", unused, &INITIAL_SNAKE_SIZE);
+	//      if (i == 4) fscanf(fp, "%s%d", unused, &INCREASE_SPEED_ON_EVERY_FOOD);
+	//   }
+	//   fclose(fp);
+	//}
 
-	arr = malloc(arraySizeX * arraySizeY * sizeof(char));   //메모리 값 오류
+	arr = malloc(arraySizeX * arraySizeY * sizeof(char));            //메모리값 오류
 	if (arr == NULL)
 	{
 		printf("cannot allocate memory\n"); _getch();
 	}
 
+	for (i = 0; i < arraySizeX * arraySizeY; i++) arr[i] = ' '; //맵 공백 초기화
+																/* Create the frame arraySizeX * arraySizeY */
+	for (i = 0; i < arraySizeX * arraySizeY; i++)
+	{
+
+		if (i < arraySizeX) arr[i] = '-';
+		else if (i % arraySizeX == 0) { arr[i] = '|'; }
+		else if (i > arraySizeX * (arraySizeY - 1)) arr[i] = '-';
+		else if (i % arraySizeX == arraySizeX - 1) arr[i] = '|';
+		//else arr[i] = ' '; 
+	}
+
+
+
 	while (1)
 	{ // new game cycle
 		srand(time(NULL));
 
-		for (i = 0; i < arraySizeX * arraySizeY; i++) arr[i] = ' ';//맵 공백 초기화
-		for (j = 0; j < 100; j++) snakePos[j] = 1;   //스네이크 포지션 1로 초기화   
+		for (j = 0; j < 100; j++) snakePos[j] = 1; //스네이크 포지션 1로 초기화
 		Dead = 0;
 		NoNewGame = 0;
 		CurrentDir = 0;
@@ -105,31 +120,33 @@ int main()
 		Key = 0;
 		if (!ChetemConfig) snakeSize = 3;
 		else snakeSize = INITIAL_SNAKE_SIZE;
-		z = 24; /* iterator for game life cycle - used to count moves after food appearence */
+		z = 24; /* iterator for game life cycle - used to count moves after food appearence         z는 음식이 사라지기 전 움직일 수 있는 최대값*/
 				/* this value should be afunction of the field size - if it si 16 -> then we have to have at least 24 moves before food dissapear*/
-				/* Input validation for speed 음식물이 사라지기 전의 한계 이동 값*/
+				/* Input validation for speed*/
 
-		while (1)   //스네이크 속도 설정
-		{
-			system("cls");
-			printf("During the game- press ESC to exit.. or DIE !\nSpeed level: (1-40)\n");
-			getchar();
-			scanf("%d", &Speed);
-			if (Speed > 40) Speed = 40;
-			if (!(Speed <= 0)) break; /* challenging levels up to speed = 30*/
-		}
 
-		/*cls 추가하기*/
+				//while (1)            // 스네이크 speed 설정 부분
+				//{
+				//   system("cls");
+				//   printf("During the game- press ESC to exit.. or DIE !\nSpeed level: (1-40)\n");
+				//   fflush(stdin);
+				//   scanf("%d", &Speed);
+				//   if (Speed > 40) Speed = 40;
+				//   if (!(Speed <= 0)) break; /* challenging levels up to speed = 30*/
+				//}
 
-		/* Initialize snakePos with size 3 */
-		//snakePos[0] = 8;  //    ------------  //Illustration, not actual represntation
-		//snakePos[1] = 8;  //    ------------
-		//snakePos[2] = 9;  //    ----***-----
-		//snakePos[3] = 8;  //    ------------
-		//snakePos[4] = 10;
-		//snakePos[5] = 8;
+				//system(cls); 추가하기  - 콘솔창 클리어
 
-		for (i = 0; i < snakeSize; i++)   //뱀 위치 랜덤설정
+
+				/* Initialize snakePos with size 3 */
+				//snakePos[0] = 8;  //    ------------  //Illustration, not actual represntation
+				//snakePos[1] = 8;  //    ------------
+				//snakePos[2] = 9;  //    ----***-----
+				//snakePos[3] = 8;  //    ------------
+				//snakePos[4] = 10;
+				//snakePos[5] = 8;
+
+		for (i = 0; i < snakeSize; i++)   //뱀 위치 랜덤 설정
 		{
 			if (i == 0)
 			{
@@ -193,29 +210,30 @@ int main()
 			}
 		}
 		fflush(stdin);
-
 		while (1)
 		{
 			z--;
-			if (z == 0) newFood = 1;   //이동 한계 초과를 했는지 체크
-									   /* Clear the screen and print the matrix and snake again */
+			if (z == 0) newFood = 1;
+			/* Clear the screen and print the matrix and snake again */
+			// z가 25번 넘어가면 음식 다시생기게하는 부분
 
 			izqdeGolemiq = 0;
 			CurrentDir = snakeDir;
 
 			/* Create the frame arraySizeX * arraySizeY */
-			for (i = 0; i < arraySizeX * arraySizeY; i++)   //맵 가장자리 생성
+			for (i = 0; i < arraySizeX * arraySizeY; i++)  //맵 가장자리 생성
 			{
 
-				if (i < arraySizeX) arr[i] = '-';
-				else if (i % arraySizeX == 0) { arr[i] = '|'; }
-				else if (i > arraySizeX * (arraySizeY - 1)) arr[i] = '-';
-				else if (i % arraySizeX == arraySizeX - 1) arr[i] = '|';
+				if (i < arraySizeX) arr[i] = '-';             //맨 위쪽 벽
+				else if (i % arraySizeX == 0) { arr[i] = '|'; } //왼쪽 벽
+				else if (i > arraySizeX * (arraySizeY - 1)) arr[i] = '-'; //맨아래 벽
+				else if (i % arraySizeX == arraySizeX - 1) arr[i] = '|'; //오른쪽 벽
 				else arr[i] = ' ';
 			}
 
 			/* Create the food - place '+' on a random coordinates different than the snake coordinates */
-			if (newFood)//이동 한계치 초과시 음식 재생성 구간 및 음식 포지션 설정(뱀과의 위치에서 일정하게 떨어져야지 생성됨)
+			if (newFood)        //이동 한계치 초과시 음식 재생성 구간 및 음식 포지션 설정(뱀과의 위치에서 일정하게 떨어져야지 생성됨)
+
 			{
 				CheckFoodCoord = 0;
 				for (;;)
@@ -236,18 +254,18 @@ int main()
 			}
 
 			/* Check snake coordinates for food for impact or walls - for food -> increase snake_size +1; - for impact and walls -> Dead = 1 */
-			for (i = 0; i < snakeSize; i++)   //충돌체크
+			for (i = 0; i < snakeSize; i++)           //충돌 체크
 			{
 				/* food check 음식과 충돌시*/
 				if ((snakePos[2 * i] == foodPos[0]) && (snakePos[2 * i + 1] == foodPos[1]))
 				{
 					newFood = 1;
 					/* the snake cannot be longer than 25 elements. */
-					if (snakeSize < 50) snakeSize++;
+					if (snakeSize < 50) snakeSize++;                         // 스네이크 길이 증가
 					izqdeGolemiq = 1;
 					if (INCREASE_SPEED_ON_EVERY_FOOD && (Speed < 40))Speed++;
 				}
-				/* wall check 벽과 충돌시 */
+				/* wall check 벽돌과 충돌시*/
 				if (!izqdeGolemiq)
 				{
 					if ((arr[snakePos[2 * snakeSize - 1] * arraySizeX + snakePos[2 * snakeSize - 2]] == '-') || (arr[snakePos[2 * snakeSize - 1] * arraySizeX + snakePos[2 * snakeSize - 2]] == '|'))
@@ -301,19 +319,19 @@ int main()
 																																								  /* Create the snake */
 			for (i = 0; i < snakeSize; i++)
 			{
-				arr[snakePos[2 * i + 1] * arraySizeX + snakePos[2 * i]] = '*';
+				arr[snakePos[2 * i + 1] * arraySizeX + snakePos[2 * i]] = '*';  // 뱀 모양
 			}
 
 			/* Create the frame arraySizeX * arraySizeY */
-			for (i = 0; i < arraySizeX * arraySizeY; i++)
-			{
+			//for (i = 0; i < arraySizeX * arraySizeY; i++)
+			//{
 
-				if (i < arraySizeX) arr[i] = '-';
-				else if (i % arraySizeX == 0) { arr[i] = '|'; }
-				else if (i > arraySizeX * (arraySizeY - 1)) arr[i] = '-';
-				else if (i % arraySizeX == arraySizeX - 1) arr[i] = '|';
-				//else arr[i] = ' ';
-			}
+			//   if (i < arraySizeX) arr[i] = '-';
+			//   else if (i % arraySizeX == 0) { arr[i] = '|'; }
+			//   else if (i > arraySizeX * (arraySizeY - 1)) arr[i] = '-';
+			//   else if (i % arraySizeX == arraySizeX - 1) arr[i] = '|';
+			//   //else arr[i] = ' ';
+			//}
 
 			/* Put the food on the matrix */
 			arr[foodPos[1] * arraySizeX + foodPos[0]] = '+';
@@ -326,12 +344,30 @@ int main()
 			{
 				for (j = 0; j < arraySizeY; j++)
 				{
-					printf("%c", arr[i * arraySizeX + j]);
+					if (arr[i * arraySizeX + j] == '-')
+					{
+						printf("-");
+					}
+					else if (arr[i * arraySizeX + j] == '|')
+					{
+						printf("■");
+					}
+					else if (arr[i * arraySizeX + j] == ' ')
+					{
+						printf(" ");
+					}
+					else if (arr[i * arraySizeX + j] == '*')
+					{
+						printf("*");
+					}
+
+					//   printf("%c", arr[i * arraySizeX + j]);
 				}
 				printf("\n");
 			}
 
-			/* if condition Dead is satisfied -> break */   //키보드로 뱀 이동
+
+			/* if condition Dead is satisfied -> break  키보드로 뱀 이동*/
 			Key = 0;
 			if (Dead) break;
 			Sleep(1000 / Speed);
@@ -383,7 +419,7 @@ int main()
 			i++;
 		}
 
-		if (Dead)   //죽었을떄
+		if (Dead)             // 뱀이 죽었을 경우
 		{
 			while (1)
 			{
@@ -403,7 +439,8 @@ int main()
 			printf("Game over\n");
 			break;
 		}
-		if (NoNewGame) break;
+		if (NoNewGame);
+
 	}
 	_getch();
 	return 0;
